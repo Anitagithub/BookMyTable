583415196ed770db0ff42f6dd9283f7a626124d1
garganshul108 <sci.agarg@gmail.com>
2019-07-31 22:03:35 +0530

    added filter

---
+++ bookmytable/src/components/restaurantFilter.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class RestaurantFilter extends Component {
+  state = {};
+  render() {
+    return <p>Filter</p>;
+  }
+}
+
+export default RestaurantFilter;
--- bookmytable/src/components/restaurants.jsx
+++ bookmytable/src/components/restaurants.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { getRestaurants } from "../dummyapi/restaurant";
 import TableRow from "./table/tableRow";
+import RestaurantFilter from "./restaurantFilter";
 
 class Restaurants extends Component {
   state = {
@@ -18,21 +19,26 @@ class Restaurants extends Component {
     const { data } = this.state;
     const count = data.length;
     return (
-      <React.Fragment>
-        <h1>Restaurants: {count}</h1>
-        <table className="table">
-          <thead>
-            <th>Name</th>
-            <th>Rating</th>
-            <th>Cuisine</th>
-          </thead>
-          <tbody>
-            {data.map(item => (
-              <TableRow key={item._id} content={item} />
-            ))}
-          </tbody>
-        </table>
-      </React.Fragment>
+      <div className="row">
+        <div className="col-3">
+          <RestaurantFilter />
+        </div>
+        <div className="col">
+          <h1>Restaurants: {count}</h1>
+          <table className="table">
+            <thead className="thead-light">
+              <th>Name</th>
+              <th>Rating</th>
+              <th>Cuisine</th>
+            </thead>
+            <tbody>
+              {data.map(item => (
+                <TableRow key={item._id} content={item} />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     );
   }
 }
