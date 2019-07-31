a67b572ee404d8fa42e7bed40d35c4e76e34f1d7
garganshul108 <sci.agarg@gmail.com>
2019-07-31 22:18:51 +0530

    incomplete

--- bookmytable/src/components/restaurants.jsx
+++ bookmytable/src/components/restaurants.jsx
@@ -1,15 +1,13 @@
 import React, { Component } from "react";
 import { getRestaurants } from "../dummyapi/restaurant";
-import TableRow from "./table/tableRow";
 import RestaurantFilter from "./restaurantFilter";
+import RestaurantsDisplay from "./restautantsDisplay";
 
 class Restaurants extends Component {
   state = {
     data: []
   };
 
-  // called once the component is initialized
-  // place for all the API calls etc
   componentDidMount() {
     let data = getRestaurants();
     this.setState({ data });
@@ -17,26 +15,13 @@ class Restaurants extends Component {
 
   render() {
     const { data } = this.state;
-    const count = data.length;
     return (
       <div className="row">
         <div className="col-3">
           <RestaurantFilter />
         </div>
         <div className="col">
-          <h1>Restaurants: {count}</h1>
-          <table className="table">
-            <thead className="thead-light">
-              <th>Name</th>
-              <th>Rating</th>
-              <th>Cuisine</th>
-            </thead>
-            <tbody>
-              {data.map(item => (
-                <TableRow key={item._id} content={item} />
-              ))}
-            </tbody>
-          </table>
+          <RestaurantsDisplay data={data} />
         </div>
       </div>
     );
---
+++ bookmytable/src/components/table/table.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Table extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        {/* <TableHeads />
+        <TableBody /> */}
+      </React.Fragment>
+    );
+  }
+}
+
+export default Table;
